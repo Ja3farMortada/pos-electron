3fedb50865a2fbf79820dc6ad22e13fb6794b149
Jaafar Mortada <mortada9190@gmail.com>
2025-09-13 15:58:15 +0300

    add print stock

--- main.js
+++ main.js
@@ -252,3 +252,29 @@ ipcMain.handle("export-excel", async (event, data) => {
         return { success: false, message: err.message };
     }
 });
+
+ipcMain.handle("print-stock", async (event, data) => {
+    // console.log(data);
+    printWindow = new BrowserWindow({
+        width: 706.95553,
+        height: 1000,
+        show: false,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    printWindow.loadFile("assets/stock.html");
+    printWindow.show();
+
+    const printOptions = {
+        silent: false, // Print without showing a dialog (optional)
+        marginsType: 0, // Set margin type (optional)
+    };
+    printWindow.webContents.on("did-finish-load", async function () {
+        await printWindow.webContents.send("printDocument", data);
+        printWindow.webContents.print(printOptions, (success) => {
+            printWindow.close();
+        });
+    });
+});
